479a2fe458c68e3c7623e6594fda0bb947bda9e4
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-08-15 23:02:46 -0300

    Update aula03.sql

--- aula03.sql
+++ aula03.sql
@@ -1,1 +1,8 @@
+create table tbl_cliente (codigo_cliente PRIMARY KEY integer, 
+nome varchar (100) not null, cidade varchar (100), endereco varchar (100));
 
+create table tbl_emprestimo ( numero_emprestimo PRIMARY KEY integer, 
+codigo_cliente varchar (100), codigo_livro varchar (100));
+					
+					
+create table tbm_titulo ( codigo_titulo PRIMARY KEY integer, titulo not null varchar (100),)
