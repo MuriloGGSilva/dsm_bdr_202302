f0da8d9d66b78b2ccd6788e792ac8f2f70ff79df
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-09-01 22:51:29 -0300

    Update aula7.sql

--- aula7.sql
+++ aula7.sql
@@ -1,1 +1,20 @@
+SELECT
+    UPPER(f.nome) AS NomeFornecedor,
+    LOWER(p.nome) AS NomePeça,
+    f.cidade AS Cidade
+FROM
+    tbl_fornecedor f
+JOIN
+    tbl_peca p ON f.cidade = p.cidade
+ORDER BY
+    NomeFornecedor, NomePeça;
+
+SELECT DISTINCT cidade, cod_fornecedor
+FROM tbl_fornecedor where cod_fornecedor is not null;
+
+SELECT p.nome, p.cor 
+FROM tbl_peca p
+INNER JOIN tbl_estoque e ON p.cod_peca = e.cod_peca
+WHERE e.cod_fornecedor = 3
+ORDER BY p.nome;
 
