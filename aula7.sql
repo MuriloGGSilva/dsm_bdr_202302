6aa0ad10e61f160459be34123618e8c0289b7f74
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-09-05 22:52:17 -0300

    Update aula7.sql

--- aula7.sql
+++ aula7.sql
@@ -9,12 +9,55 @@ JOIN
 ORDER BY
     NomeFornecedor, NomePeça;
 
+
 SELECT DISTINCT cidade, cod_fornecedor
 FROM tbl_fornecedor where cod_fornecedor is not null;
+
 
 SELECT p.nome, p.cor 
 FROM tbl_peca p
 INNER JOIN tbl_estoque e ON p.cod_peca = e.cod_peca
 WHERE e.cod_fornecedor = 3
 ORDER BY p.nome;
+
+
+SELECT  f.nome , f.cidade 
+FROM  tbl_fornecedor f
+JOIN  tbl_estoque e ON f.cod_fornecedor = e.cod_fornecedor
+JOIN  tbl_peca p ON e.cod_peca = p.cod_peca
+WHERE p.cod_peca = 1
+GROUP BY f.nome, f.cidade
+HAVING  SUM(e.quantidade) > 10;
+
+
+SELECT SUM(e.quantidade) AS quantidade_total
+FROM tbl_estoque e
+JOIN tbl_peca p ON e.cod_peca = p.cod_peca
+WHERE  p.cod_peca = 1;
+
+
+SELECT  AVG(p.preco) AS media_precos
+FROM  tbl_peca p
+JOIN  tbl_estoque e ON p.cod_peca = e.cod_peca
+WHERE  e.cod_fornecedor = 3;
+
+
+SELECT MAX(p.preco) AS peca_mais_cara,
+MIN(p.preco) AS peca_mais_barata
+FROM  tbl_peca p
+JOIN  tbl_estoque e ON p.cod_peca = e.cod_peca;
+
+
+SELECT COUNT(*) AS quantidade_de_pecas
+FROM tbl_peca;
+
+
+SELECT COUNT(DISTINCT cidade) AS quantidade_de_cidades
+FROM tbl_peca;
+
+SELECT ROUND(AVG(preco), 1) AS media_precos
+FROM tbl_peca;
+
+
+
 
